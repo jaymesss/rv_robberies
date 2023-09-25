400242b134d1418088a58598be959c0fb0b761ac
James <hoffmannjamesjr@gmail.com>
2023-09-24 19:15:47 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,24 @@ description 'Multiple leveling robberies for QBCore'
 version '1.0.0'
 client_scripts {
     'client/client.lua',
-    'client/vangelico.lua'
+    'client/fleeca.lua',
+    'client/vangelico.lua',
+    'client/paleto.lua',
+    'client/pacific.lua',
 }
 server_scripts {
     'server/server.lua',
+    'server/fleeca.lua',
     'server/vangelico.lua',
+    'server/paleto.lua',
+    'server/pacific.lua',
     '@oxmysql/lib/MySQL.lua'
 }
 shared_script {
     '@ox_lib/init.lua',
     'shared/config.lua',
+    'shared/locations.lua',
+    'shared/missions.lua',
     'locale/en.lua'
 }
 escrow_ignore {
